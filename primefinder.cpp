e217e49093e4940d2d8b48d335a42249c2535120
Jon Leithe <jon@jonleithe.no>
2024-09-07 17:23:51 +0200

    run and weep

--- primefinder.cpp
+++ primefinder.cpp
@@ -37,6 +37,7 @@ void printUsage(char* _progName)
 	cout << "\t\t-o (--stop)\tnumber to stop at (default = 23)" << endl;
 	cout << "\t\t-p (--print)\tprint primes to screen" << endl;
 	cout << "\t\t-c (--col)\tnumber of columns to print (default = 5)" << endl;
+	cout << "\t\t   (--algo)\t[atkin] specify if you want sieve of Atkin (default = sieve of Eratosthenes)" << endl;
 	cout << "\t\t-h (--help)\tprint this help text" << endl;
 }
 
@@ -47,8 +48,131 @@ void printUsage(char* _progName)
 //
 struct my_numpunct : std::numpunct<char> {
 	// We want to group in numbers of three
-	std::string do_grouping() const {return "\03";}
+	string do_grouping() const {return "\03";}
 };
+
+
+/*
+ * just preverving a copy of the original from web
+
+void sieveOfAtkin(int lmt) {
+
+	cout << "Atkin" << endl;
+
+   if (lmt > 2)
+      cout << 2 << " ";
+   if (lmt > 3)
+      cout << 3 << " ";
+   bool sieve[lmt];
+   for (int i = 0; i < lmt; i++)
+      sieve[i] = false;
+   for (int a = 1; a * a < lmt; a++) {
+      for (int b = 1; b * b < lmt; b++) {
+         // Main part of Sieve of Atkin
+         int n = (4 * a* a) + (b * b);
+         if (n <= lmt && (n % 12 == 1 || n % 12 == 5))
+            sieve[n] ^= true;
+            n = (3 * a * a) + (b * b);
+         if (n <= lmt && n % 12 == 7)
+            sieve[n] ^= true;
+            n = (3 * a * a) - (b * b);
+         if (a > b && n <= lmt && n % 12 == 11)
+            sieve[n] ^= true;
+      }
+   }
+   for (int r = 5; r * r < lmt; r++) {
+      if (sieve[r]) {
+         for (int i = r * r; i < lmt; i += r * r)
+            sieve[i] = false;
+      }
+   }
+   for (int x = 5; x < lmt; x++)
+      if (sieve[x])
+         cout << x << " ";
+}
+*/
+
+
+
+void sieveOfAtkin(unsigned long long _start, unsigned long long _stop, bool _print, short _columns)
+{
+	bool				newColumn = false;
+	short				column = 1;
+	unsigned long long	counter = 0;
+
+	cout << "Atkin" << endl;
+
+
+	if(_print && _start == 2){
+		if (_stop > 2)
+			cout << 2;
+		if (_stop > 3)
+			cout << "\t\t" <<  3;
+
+		counter	+= 2;
+		column	+= 2;
+	}
+
+	if(_print && _start == 3){
+			if (_stop > 3)
+				cout <<  3;
+
+			counter	+= 1;
+			column	+= 1;
+		}
+
+
+	// create as vectors to better handle memory issues
+	//bool sieve[_stop];
+	vector<bool> sieve(_stop+1, true);
+	sieve[0] = sieve[1] = false;
+
+	for (unsigned long long i = 0; i <= _stop; i++)
+		sieve[i] = false;
+
+	for (unsigned long long a = 1; a * a <= _stop; a++){
+		for (unsigned long long b = 1; b * b <= _stop; b++){
+			// Main part of Sieve of Atkin
+			unsigned long long n = (4 * a* a) + (b * b);
+			if (n <= _stop && (n % 12 == 1 || n % 12 == 5))
+				sieve[n] = !sieve[n];	//sieve[n] ^= true;
+			n = (3 * a * a) + (b * b);
+			if (n <= _stop && n % 12 == 7)
+				sieve[n] = !sieve[n];	//sieve[n] ^= true;
+			n = (3 * a * a) - (b * b);
+			if (a > b && n <= _stop && n % 12 == 11)
+				sieve[n] = !sieve[n];	//sieve[n] ^= true;
+		}
+	}
+	for (unsigned long long r = 5; r * r <= _stop; r++) {
+		if (sieve[r]){
+			for (unsigned long long i = r * r; i <= _stop; i += r * r)
+				sieve[i] = false;
+			}
+	}
+	if(_print){
+		for (unsigned long long x = 2; x <= _stop; x++){
+			if(sieve[x]){
+				counter++;
+				if(column > 1)
+					cout << "\t\t";
+				cout << x;
+				column++;
+
+				if(column > _columns){
+					column = 1;
+					cout << endl;
+				}
+
+			}
+		}
+	}
+
+	std::locale loc (std::cout.getloc(),new my_numpunct);
+	std::cout.imbue(loc);
+
+	cout << endl << "Number of primes between " << _start << " and " << _stop << " is " << counter << endl;
+}
 
 
 
@@ -58,6 +182,7 @@ void sieveOfEratosthenes(unsigned long long _start, unsigned long long _stop, bo
 	short				column = 1;
     unsigned long long	counter = 0;
 
+    cout << "Eratosthenes" << endl;
 
     vector<bool> is_prime(_stop+1, true);
     is_prime[0] = is_prime[1] = false;
@@ -100,6 +225,8 @@ void sieveOfEratosthenes(unsigned long long _start, unsigned long long _stop, bo
 int main(int argc, char *argv[])
 {
 	bool				print = false;
+	bool				atkin = false;
+
 	int					columns = 5;
 	unsigned long long	start, stop;
 
@@ -118,6 +245,7 @@ int main(int argc, char *argv[])
 					{"stop",	required_argument,	NULL,	'o'},
 					{"printz",	required_argument,	NULL,	'p'},
 					{"col",		required_argument,	NULL,	'c'},
+					{"algo",	required_argument,	NULL,	1},
 					{"help",	no_argument,		NULL,	'h'},
 					{0, 0, 0, 0}
 	};	//End of getopt()-variables
@@ -147,6 +275,12 @@ int main(int argc, char *argv[])
 							print = true;
 							break;
 						}
+			case 1:		{
+							if(!string(optarg).compare("atkin"))
+								atkin = true;
+
+							break;
+						}
 			default:	{
 							cout << "Switch default\n" << endl;
 							break;
@@ -154,10 +288,10 @@ int main(int argc, char *argv[])
 		}	//end of getopt() switch statement
 	}	// end of while-loop
 
-	std::locale loc (std::cout.getloc(),new my_numpunct);
-	std::cout.imbue(loc);
+	locale loc (cout.getloc(),new my_numpunct);
+	cout.imbue(loc);
 
-	sieveOfEratosthenes(start, stop, print, columns);
+	atkin ? sieveOfAtkin(start, stop, print, columns) : sieveOfEratosthenes(start, stop, print, columns);
 
 	if(argc == 1){
 		cout << endl;
